1268cabfafb2e71be403acbfaf143781b3e98f5c
fourbarman <maks.java@yandex.ru>
2024-03-04 10:54:47 +0400

    Added Util class? returning Properties for connection.

--- src/com/fourbarman/jdbc/starter/JdbcRunner.java
+++ src/com/fourbarman/jdbc/starter/JdbcRunner.java
@@ -4,11 +4,20 @@ import com.fourbarman.jdbc.starter.util.ConnectionManager;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
     public static void main(String[] args) {
-        try(Connection connection = ConnectionManager.open()) {
+        String sql = """
+                    CREATE TABLE IF NOT EXISTS info(
+                    id SERIAL PRIMARY KEY ,
+                    data text NOT NULL );
+                """;
+        try(Connection connection = ConnectionManager.open();
+            Statement statement = connection.createStatement()) {
             System.out.println(connection.getTransactionIsolation());
+            boolean executeResult = statement.execute(sql);
+            System.out.println(executeResult);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
