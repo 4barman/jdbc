09ff23a2de52ae6ccdbc6448228c75743a46756b
fourbarman <maks.java@yandex.ru>
2024-03-04 11:20:28 +0400

    executeQuery for SELECT. ResultSet example.

--- src/com/fourbarman/jdbc/starter/JdbcRunner.java
+++ src/com/fourbarman/jdbc/starter/JdbcRunner.java
@@ -3,20 +3,30 @@ package com.fourbarman.jdbc.starter;
 import com.fourbarman.jdbc.starter.util.ConnectionManager;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JdbcRunner {
     public static void main(String[] args) {
         String sql = """
-                    INSERT INTO info(data)
-                    VALUES ('test1'), ('test2'), ('test3'), ('test4');
+                    SELECT *
+                    FROM ticket;
                 """;
         try(Connection connection = ConnectionManager.open();
             Statement statement = connection.createStatement()) {
             System.out.println(connection.getTransactionIsolation());
-            int executeResult = statement.executeUpdate(sql);
+
+            ResultSet executeResult = statement.executeQuery(sql);
+
             System.out.println(executeResult);
+
+            while(executeResult.next()) {
+                System.out.println(executeResult.getLong("id"));
+                System.out.println(executeResult.getString("passenger_no"));
+                System.out.println(executeResult.getBigDecimal("cost"));
+                System.out.println("-----------");
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
