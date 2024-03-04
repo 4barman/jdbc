73136e81e7f971cb699df1e453671ba1aee422a6
fourbarman <maks.java@yandex.ru>
2024-03-04 14:45:43 +0400

    PreparedStatement. FetchSize, MaxRows, Timeout.

--- src/com/fourbarman/jdbc/starter/JdbcRunner.java
+++ src/com/fourbarman/jdbc/starter/JdbcRunner.java
@@ -2,31 +2,61 @@ package com.fourbarman.jdbc.starter;
 
 import com.fourbarman.jdbc.starter.util.ConnectionManager;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcRunner {
     public static void main(String[] args) {
-        String flightId = "2";
-        List<Long> listByFlightId = getListByFlightId(flightId);
-        System.out.println(listByFlightId);
+//        Long flightId = 2L;
+//        List<Long> listByFlightId = getListByFlightId(flightId);
+//        System.out.println(listByFlightId);
+        System.out.println(getFlightsBetween(LocalDate.of(2020, 10, 1).atStartOfDay(), LocalDate.now().atStartOfDay()));
     }
 
-    private static List<Long> getListByFlightId(String flightId) {
+    private static List<Long> getFlightsBetween(LocalDateTime start, LocalDateTime end) {
+        String sql = """
+                SELECT id
+                FROM flight
+                WHERE departure_date BETWEEN ? AND ?;
+                """;
+        List<Long> result = new ArrayList<>();
+        try (Connection connection = ConnectionManager.open();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setFetchSize(50);
+            preparedStatement.setQueryTimeout(10);
+            preparedStatement.setMaxRows(100);
+
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(start));
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(end));
+            System.out.println(preparedStatement);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()) {
+                result.add(resultSet.getLong("id"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    private static List<Long> getListByFlightId(Long flightId) {
         String sql = """
                 SELECT id
                 FROM ticket
-                WHERE flight_id = %s; DROP TABLE info;
-                """.formatted(flightId);
+                WHERE flight_id = ?
+                """;
         List<Long> list = new ArrayList<>();
         try (Connection connection = ConnectionManager.open();
-        Statement statement = connection.createStatement()) {
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
-            ResultSet resultSet = statement.executeQuery(sql);
+            preparedStatement.setLong(1, flightId);
+            ResultSet resultSet = preparedStatement.executeQuery();
             while(resultSet.next()) {
                 list.add(resultSet.getObject("id", Long.class)); //NULL SAFE
             }
